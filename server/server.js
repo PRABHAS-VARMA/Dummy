df8461755df5287c9299138b65c74a71f605449e
PRABHAS VARMA <prabhasvarma017@gmail.com>
2024-04-05 13:15:40 +0530

    Connected mongoDB database with the backend

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PUBLIC_PORT || 3000;
+
 const dotenv = require('dotenv');
 dotenv.config();
+
+const mongoDbUri = process.env.MongoDB_URL;
+
+//Connect mongoDB
+async function Connection() {
+  await mongoose.connect(mongoDbUri);
+  console.log("connected to DB");
+}
+
 app.get('/ping', (req,res)=> {
   res.json({message:"pong"});
 })
-if (require.main === module) {
+
+Connection().then(() => {
   app.listen(port, () => {
     console.log(`🚀 server running on PORT: ${port}`);
   });
-}
-module.exports = app;
+});
